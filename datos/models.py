25b9591724bcff055a312cfc74fecd680e723451
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-12-09 10:41:17 -0300

    agrego models

--- datos/models.py
+++ datos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Registro(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
